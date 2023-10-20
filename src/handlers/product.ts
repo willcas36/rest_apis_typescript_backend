9fea4a0b72af9be97790074aa6649dba2549e7fc
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-20 17:10:36 -0600

    23

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,14 +1,11 @@
 import { Request, Response } from 'express'
-import { validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
 export const createProduct = async (req : Request, res : Response) => {
-
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+    try {
+        const product = await Product.create(req.body)
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
     }
-
-    const product = await Product.create(req.body)
-    res.json({data: product})
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
 import { createProduct } from './handlers/product'
+import { handleInputErrors } from './middleware'
 
 const router = Router()
 
@@ -17,6 +18,7 @@ router.post('/',
         .isNumeric().withMessage('Valor no válido')
         .notEmpty().withMessage('El precio de Producto no puede ir vacio')
         .custom(value => value > 0).withMessage('Precio no válido'),
+    handleInputErrors,
     createProduct
 )
 
