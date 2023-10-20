632d681760342187263d887405351c3d2e4c8cba
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-20 12:21:03 -0600

    18

---
+++ src/models/Product.model.ts
@@ -0,0 +1,24 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript'
+
+@Table({
+    tableName: 'products'
+})
+
+class Product extends Model {
+    @Column({
+        type: DataType.STRING(100)  
+    })
+    name: string
+
+    @Column({
+        type: DataType.FLOAT(6, 2)
+    })
+    price: number
+
+    @Column({
+        type: DataType.BOOLEAN
+    })
+    availability: boolean
+}
+
+export default Product
