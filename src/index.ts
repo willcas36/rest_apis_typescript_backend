40fafb812a4c6938fd5ef77248d8ca76a4f92722
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-19 19:05:01 -0600

    17

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import colors from 'colors'
 import server from './server'
 
+const port = process.env.PORT || 4000
 
-server.listen(4000, () => {
-    console.log(`REST API en el puerto 4000`)
+server.listen(port, () => {
+    console.log( colors.cyan.bold( `REST API en el puerto ${port}`))
 })
