c7dca1432cda662ca41454543d9be6de38b5bfeb
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-20 18:43:06 -0600

    25

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
-import { body } from 'express-validator'
-import { createProduct, getProducts } from './handlers/product'
+import { body, param } from 'express-validator'
+import { createProduct, getProductById, getProducts } from './handlers/product'
 import { handleInputErrors } from './middleware'
 
 const router = Router()
 
 // Routing
 router.get('/', getProducts)
+router.get('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    getProductById
+)
 
 router.post('/', 
     // Validación
