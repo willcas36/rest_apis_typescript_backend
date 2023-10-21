432188b3c4a830ed6756fb3643eaae8b66bf2699
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-20 18:16:47 -0600

    24

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
-import { createProduct } from './handlers/product'
+import { createProduct, getProducts } from './handlers/product'
 import { handleInputErrors } from './middleware'
 
 const router = Router()
 
 // Routing
-router.get('/', (req, res) => {
-    res.json('Desde GET')
-})
+router.get('/', getProducts)
 
 router.post('/', 
     // Validación
