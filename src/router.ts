3494d3b6653147aa9971051a5f4b66f55bb8f92c
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-21 18:21:29 -0600

    28

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { body, param } from 'express-validator'
-import { createProduct, getProductById, getProducts, updateAvailability, updateProduct } from './handlers/product'
+import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from './handlers/product'
 import { handleInputErrors } from './middleware'
 
 const router = Router()
@@ -26,7 +26,7 @@ router.post('/',
 )
 
 router.put('/:id', 
-    // Validación
+    param('id').isInt().withMessage('ID no válido'),
     body('name')
         .notEmpty().withMessage('El nombre de Producto no puede ir vacio'),
     body('price')
@@ -39,10 +39,16 @@ router.put('/:id',
     updateProduct
 )
 
-router.patch('/:id', updateAvailability)
+router.patch('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    updateAvailability
+)
 
-router.delete('/', (req, res) => {
-    res.json('Desde DELETE')
-})
+router.delete('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    deleteProduct
+)
 
 export default router
